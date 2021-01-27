1d4a2d82b2f1b3db5986d070ffadca86411a33be
peckas13 <alex13pks@gmail.com>
2021-01-26 21:15:49 -0600

    Creadas las rutas y componentes principales

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RegisterComponent } from './home/register/register.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', redirectTo: 'register', pathMatch: 'full'
+  },
+  {
+    path: 'register', component: RegisterComponent 
+  },
+  {
+    path: '**', component: NotFoundComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
