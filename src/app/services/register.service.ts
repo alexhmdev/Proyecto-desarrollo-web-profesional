ba22918ae90c50fa35e235c3e9d1263dd0d745f4
AlexHM <alejandro.herrera@utags.edu.mx>
2021-01-27 16:44:51 -0600

    Agregados los servicios a github

---
+++ src/app/services/register.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+import { environment } from 'src/environments/environment.prod'
+@Injectable({
+  providedIn: 'root'
+})
+export class RegisterService {
+  url = `${environment.url}security/create_account`
+  constructor(private http: HttpClient) { }
+
+
+  postRegister(body: any) {
+    return this.http.post(this.url, body)
+  }
+}
