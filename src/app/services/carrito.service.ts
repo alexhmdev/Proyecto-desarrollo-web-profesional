6892e534952095923df5bd05c53325b0d58cef4f
Leticia Guadalupe Moreno Martinez <180129@utags.edu.mx>
2021-03-29 20:07:55 -0600

    Se agregaron los servicios de PUT y DELETE para el carrito

--- src/app/services/carrito.service.ts
+++ src/app/services/carrito.service.ts
@@ -9,8 +9,10 @@ export class CarritoService {
   url = `${environment.url}/cart/add_item`;
   urlB = `${environment.url}/cart/get_details`;
   urlC = `${environment.url}/cart/remove_all`;
+  urlD = `${environment.url}/cart/update_item`;
+  urlE = `${environment.url}/cart/remove_item`;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) { }
 
   postCart(body: any) {
     return this.http.post(this.url, body).toPromise();
@@ -22,5 +24,13 @@ export class CarritoService {
 
   deleteAll(session_id: any) {
     return this.http.delete(this.urlC, session_id).toPromise();
+  }
+
+  putCart(body: any) {
+    return this.http.put(this.urlD, body).toPromise();
+  }
+
+  deleteCart(body: any) {
+    return this.http.delete(this.urlE, body).toPromise();
   }
 }
